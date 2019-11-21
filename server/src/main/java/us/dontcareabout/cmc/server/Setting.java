eded2d6ad55bc622ab76e61b585b4466c706350a
MontyPan <monty.pan@dontcareabout.us>
2019-11-21 11:46:12 +0800

    增加：Setting 與預設設定檔

---
+++ server/src/main/java/us/dontcareabout/cmc/server/Setting.java
@@ -0,0 +1,19 @@
+package us.dontcareabout.cmc.server;
+
+import us.dontcareabout.java.common.DoubleProperties;
+
+public class Setting extends DoubleProperties {
+	private static String workspace;
+
+	public Setting() {
+		super("dev-setting.xml", "CMC.xml");
+	}
+
+	public String workspace() {
+		if (workspace == null) {
+			workspace = getProperty("workspace");
+		}
+
+		return workspace;
+	}
+}
