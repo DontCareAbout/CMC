811c6bc14548c98537a214ed4ff86375e694cfab
MontyPan <monty.pan@dontcareabout.us>
2019-11-21 21:51:35 +0800

    museum 相關初始化集中至 Service

---
+++ server/src/main/java/us/dontcareabout/cmc/server/museum/Service.java
@@ -0,0 +1,15 @@
+package us.dontcareabout.cmc.server.museum;
+
+import us.dontcareabout.cmc.common.shared.Museum;
+import us.dontcareabout.cmc.server.Setting;
+import us.dontcareabout.cmc.server.museum.collection.Met;
+import us.dontcareabout.cmc.server.museum.staff.CollectionManager;
+
+public class Service {
+	private static final Setting setting = new Setting();
+
+	public static final CollectionManager collection = new CollectionManager(setting.workspace());
+	static {
+		collection.recruit(Museum.Met, new Met());
+	}
+}
