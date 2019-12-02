d1ceca6be64fac455252ad20396f9ce4aa8d727d
MontyPan <monty.pan@dontcareabout.us>
2019-12-02 22:26:13 +0800

    增加：chrome folder 設定

--- server/src/main/java/us/dontcareabout/cmc/server/Setting.java
+++ server/src/main/java/us/dontcareabout/cmc/server/Setting.java
@@ -3,17 +3,15 @@ package us.dontcareabout.cmc.server;
 import us.dontcareabout.java.common.DoubleProperties;
 
 public class Setting extends DoubleProperties {
-	private static String workspace;
-
 	public Setting() {
 		super("dev-setting.xml", "CMC.xml");
 	}
 
 	public String workspace() {
-		if (workspace == null) {
-			workspace = getProperty("workspace");
-		}
+		return getProperty("workspace");
+	}
 
-		return workspace;
+	public String chromeFolder() {
+		return getProperty("chromeFolder");
 	}
 }
