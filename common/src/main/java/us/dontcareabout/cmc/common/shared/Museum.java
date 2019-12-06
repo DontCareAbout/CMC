d1ccc2970698291b8fd3e0b86507aa43901300d5
MontyPan <monty.pan@dontcareabout.us>
2019-12-07 01:43:24 +0800

    增加：title

--- common/src/main/java/us/dontcareabout/cmc/common/shared/Museum.java
+++ common/src/main/java/us/dontcareabout/cmc/common/shared/Museum.java
@@ -1,12 +1,14 @@
 package us.dontcareabout.cmc.common.shared;
 
 public enum Museum {
-	Met("https://www.metmuseum.org/art/collection/search/"),
+	Met("Met", "https://www.metmuseum.org/art/collection/search/"),
 	;
 
+	public final String title;
 	public final String url;
 
-	private Museum(String url) {
+	private Museum(String title, String url) {
+		this.title = title;
 		this.url = url;
 	}
 }
